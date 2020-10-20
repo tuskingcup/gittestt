ed6baf320b160c402dbd50d3dfdbd9ef400dca13
rapeepat <tuskung8030@gmail.com>
2020-10-20 17:43:14 +0700

    Feature2

--- src/gittestt/GitTestt.java
+++ src/gittestt/GitTestt.java
@@ -4,13 +4,19 @@ public class GitTestt {
 
     public static void main(String[] args) {
         sayHello();
-        
+        sayHi();
         dd();
     }
 
     public static void sayHello() {
         System.out.println("Hello");
     }
-    public static void dd(){
-        System.out.println("Good Bye");}
+
+    public static void sayHi() {
+        System.out.println("Hi");
+    }
+
+    public static void dd() {
+        System.out.println("Good Bye");
+    }
 }
