172354fc8c97f1606b44fd6fb581a933ce0c398c
rapeepat <tuskung8030@gmail.com>
2020-10-20 17:36:38 +0700

    Feature1

--- src/gittestt/GitTestt.java
+++ src/gittestt/GitTestt.java
@@ -1,11 +1,16 @@
-
 package gittestt;
 
 public class GitTestt {
 
     public static void main(String[] args) {
         sayHello();
+        
+        dd();
     }
-    public static void sayHello(){
-        System.out.println("Hello");}
+
+    public static void sayHello() {
+        System.out.println("Hello");
+    }
+    public static void dd(){
+        System.out.println("Good Bye");}
 }
