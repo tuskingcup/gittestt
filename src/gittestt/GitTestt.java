1a544531129ba808a23a36455b244a4d908f91b3
rapeepat <tuskung8030@gmail.com>
2020-10-20 15:12:22 +0700

    First Commit

---
+++ src/gittestt/GitTestt.java
@@ -0,0 +1,11 @@
+
+package gittestt;
+
+public class GitTestt {
+
+    public static void main(String[] args) {
+        sayHello();
+    }
+    public static void sayHello(){
+        System.out.println("Hello");}
+}
